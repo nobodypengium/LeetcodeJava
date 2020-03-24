f3694d1be556bdb7af7dd1ce6cfe0440196ae289
nobodypengium <nobodypengium@gmail.com>
2020-03-25 00:16:24 +0800

    239. Sliding Window Maximum

--- src/stack/easy/SlidingWindowMaximum.java
+++ src/stack/easy/SlidingWindowMaximum.java
@@ -1,5 +1,8 @@
 package stack.easy;
 
+import java.sql.Array;
+import java.util.ArrayDeque;
+
 public class SlidingWindowMaximum {
     //自己搞得暴力法，两层循环，外层遍历数组，内层遍历k
 //    public int[] maxSlidingWindow(int[] nums, int k) {
@@ -17,5 +20,33 @@ public class SlidingWindowMaximum {
 //        }
 //        return res;
 //    }
-
+    ArrayDeque<Integer> deque = new ArrayDeque<>();
+    int nums[];
+    public void cleanQueue(int i,int k){
+        if(!this.deque.isEmpty()&&this.deque.getFirst()<=i-k)
+            deque.removeFirst();
+        while(!deque.isEmpty()&&nums[i]>nums[deque.getLast()])
+            deque.removeLast();
+    }
+    public int[] maxSlidingWindow(int[] nums, int k) {
+        if(nums.length*k==0) return new int[0];
+        if(nums.length==1) return nums;
+        this.nums=nums;
+        //初始化
+        int max_index = 0;
+        for(int i=0;i<k;i++){
+            cleanQueue(i,k);
+            this.deque.addLast(i);
+            if(nums[i]>nums[max_index])
+                max_index=i;
+        }
+        int[] output = new int[nums.length-k+1];
+        output[0]=nums[max_index];
+        for(int i=k;i<nums.length;i++){
+            cleanQueue(i,k);
+            this.deque.addLast(i);
+            output[i-k+1]=nums[deque.getFirst()];
+        }
+        return output;
+    }
 }
