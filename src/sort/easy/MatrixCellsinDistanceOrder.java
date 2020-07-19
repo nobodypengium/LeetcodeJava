ff183871d67120062ab0badf4725559d2c67afba
nobodypengium <nobodypengium@gmail.com>
2020-07-19 23:53:16 +0800

    1030. Matrix Cells in Distance Order

--- src/sort/easy/MatrixCellsinDistanceOrder.java
+++ src/sort/easy/MatrixCellsinDistanceOrder.java
@@ -1,5 +1,7 @@
 package sort.easy;
 
+import java.util.*;
+
 public class MatrixCellsinDistanceOrder {
     public int[][] allCellsDistOrder(int R, int C, int r0, int c0) {
         int[][] result = new int[R*C][2];
@@ -62,5 +64,70 @@ public class MatrixCellsinDistanceOrder {
             }
         }
         return result;
+    }
+    //还是返回一个[R*C][2]的二维数组，但是是直接调用快排，快排可以放Comparator进去来指定比较的方法，这样就不限于能比较一维数组，也能比较二维数组了
+    public int[][] allCellsDistOrder2(int R, int C, int r0, int c0) {
+        // 导入二维数组
+        int[][] result = new int[R*C][2];
+        int count = 0;
+        for(int r=0;r<R;r++){
+            for(int c=0;c<C;c++) {
+                result[count][0]=r;
+                result[count][1]=c;
+                count++;
+            }
+        }
+        // 由Comparator定义
+        Arrays.sort(result, new Comparator<int[]>() {
+            @Override
+            public int compare(int[] o1, int[] o2) {
+                int dist1 = Math.abs(r0-o1[0])+Math.abs(c0-o1[1]);
+                int dist2 = Math.abs(c0-o2[0])+Math.abs(c0-o2[1]);
+                return dist1-dist2;
+            }
+        });
+        return result;
+    }
+    //使用桶排序（实际上没有排序），每个桶里放同样dist的点，最后遍历所有桶。
+    public int[][] allCellsDistOrder3(int R, int C, int r0, int c0) {
+        // 导入二维数组
+        int[][] result = new int[R*C][2];
+        //找出桶间隔，本题中每个距离都有一个桶，实际上可以根据数据中的最大最小值再行计算
+        int maxDist = Math.max(R-r0-1,r0)+Math.max(C-c0-1,c0);
+        ArrayList<LinkedList<Pos>> buckets = new ArrayList<>(maxDist+1);//ArrayList理解为数组，这么做相当于对象数组
+        //注意ArrayList的操作，刚创建好只是一个空数组，东西还得一个个往里放，比如linked list
+        for(int i=0;i<maxDist+1;i++)
+            buckets.add(new LinkedList<>());
+        for(int r=0;r<R;r++){
+            for(int c=0;c<C;c++){
+                int dist = Math.abs(r-r0)+Math.abs(c-c0);
+                Pos pos = new Pos(r,c);
+                buckets.get(dist).add(pos);//接到对应桶的LinkedList
+            }
+        }
+
+        // 顺序遍历所有桶，输出每个桶中的所有东西
+        int count=0;
+        for(int i=0;i<maxDist+1;i++){
+            LinkedList<Pos> bucket = buckets.get(i);
+            Iterator<Pos> it = bucket.iterator();
+            while(it.hasNext()){
+                Pos p = it.next();
+                result[count][0]=p.r;
+                result[count][1]=p.c;
+                count++;
+            }
+        }
+
+        return result;
+    }
+
+    private static class Pos{
+        int r;
+        int c;
+        Pos(int r,int c){
+            this.r=r;
+            this.c=c;
+        }
     }
 }
